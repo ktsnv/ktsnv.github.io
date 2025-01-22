b1aa3d77c0aff880961b67b4e8993a306add3298
Henry Lee <104782108+ktsnv@users.noreply.github.com>
2025-01-22 14:13:50 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,5 +1,5 @@
 let repos;
-
+const container = document.querySelector("#project_container");
 function preload() {
   repos = loadJSON('https://api.github.com/users/ktsnv/repos');
 }
@@ -8,6 +8,9 @@ function setup() {
   let size = Object.keys(repos).length;
   let i = 0;
   while (i<size) {
+    const br = document.createElement("br");
+    container.append(br);
+    container.append(repos[i].full_name);
     console.log(repos[i].full_name)
     i++;
   }
